421e565e04827da12ff8077ee80c43efb672fc17
Ferindra N <fnugrahendi@gmail.com>
2016-11-13 15:31:52 +0700

    create widget class

---
+++ class.fn-widget.php
@@ -0,0 +1,59 @@
+<?php
+//~ init widget
+class FN_Widget extends WP_Widget {
+
+	function __construct() {
+		parent::__construct(
+			'fn_widget', // Base ID
+			esc_html__( 'FN Simple List', 'text_domain' ), // Name
+			array( 'description' => esc_html__( 'List widget', 'text_domain' ), ) // Args
+		);
+	}
+
+	//~ Frontend
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		}
+		$table = $GLOBALS['wpdb']->prefix.'fnlist';
+		$results = $GLOBALS['wpdb']->get_results('SELECT * FROM '.$table.'');
+		$rowNum = $GLOBALS['wpdb']->num_rows;
+		$n = 0;
+		foreach($results as $record=>$row){
+			$order = $row->order;
+			$name = $row->name;
+			//~ $str = $results[$id]->testimoni;
+			echo esc_html__('#'.$order.'  ', 'text_domain' );
+			echo esc_html__($name, 'text_domain' );
+			echo "<br>";
+		}
+		echo $args['after_widget'];
+	}
+
+	//~ Backend
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	//~ Sanitize value
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+}
+//~ register widget
+function register_fn_widget() {
+    register_widget( 'FN_Widget' );
+}
+add_action( 'widgets_init', 'register_fn_widget' );
+
+?>
