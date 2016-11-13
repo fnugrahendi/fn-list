415d859a07a9179d0922e4d2a3cb7aa14d197135
Ferindra N <fnugrahendi@gmail.com>
2016-11-13 12:46:52 +0700

    add backend menu

--- fn-list.php
+++ fn-list.php
@@ -7,4 +7,28 @@ Version: 1.0
 Author: Ferindra Nugrahendi
 */
 
+//~ add into admin menu
+add_action( 'admin_menu', 'fn_admin_menu' );
+
+function fn_admin_menu() {
+	add_menu_page( 
+		'List', 
+		'Simple List', 
+		'manage_options', 
+		'fn-list.php', 
+		'slist_admin_page', 
+		'dashicons-editor-ol', 
+		14
+	);
+}
+
+//~ admin page function
+function slist_admin_page(){
+	?>
+	<div class="wrap">
+		<h1>Simple List</h2>
+	</div>
+	<?php
+}
+
 ?>
