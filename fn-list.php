0c52c2e72c2b4d113f7f5e130e8b7b10522663b7
Ferindra N <fnugrahendi@gmail.com>
2016-11-13 13:12:40 +0700

    add input form

--- fn-list.php
+++ fn-list.php
@@ -26,9 +26,56 @@ function fn_admin_menu() {
 function slist_admin_page(){
 	?>
 	<div class="wrap">
-		<h1>Simple List</h2>
-	</div>
+		<h1>Simple List</h2><br>
 	<?php
+	$table = $GLOBALS['wpdb']->prefix.'fnlist';
+	$results = $GLOBALS['wpdb']->get_results('SELECT * FROM '.$table.'');
+	?>
+	<table class="wp-list-table fixed widefat stripped">
+		<thead>
+			<tr>
+				<td style="width: 55px">Order</td>
+				<td style="width: 155px">Name</td>
+				<td>Description</td>
+				<td style="width: 50px">Action</td>
+			</tr>
+		</thead>
+		<tbody>
+	<?php
+	foreach($results as $record => $row){
+		echo '<tr>';
+		echo '<td>'.$row->order.'</td>';
+		echo '<td>'.$row->name.'</td>';
+		echo '<td>'.$row->description.'</td>';
+		echo '<td style="text-align: center">';
+		echo '<a href="#" onclick="test('.$row->id.')">';
+		echo '<span class="dashicons dashicons-trash"></span>';
+		echo '</a>';
+		echo '</td>';
+		echo '</tr>';
+		echo '</form>';
+	}
+	echo '</tbody>';
+	echo '</table>';
+	
+	echo '<div class="input_list" style="margin: 55px 0px;">';
+	echo '<h3>Add New List</h3>';
+	echo '<form action="'.esc_url($_SERVER['REQUEST_URI']).'" method = "post">';
+	echo '<p>';
+	echo 'Order (*) <br>';
+	echo '<input type="number" name="fn-order" value="'.(isset($_POST["fn-order"]) ? esc_attr($_POST["fn-order"]):'').'" size="4">';
+	echo '</p><p>';
+	echo 'Name (*) <br>';
+	echo '<input type="text" name="fn-name" value="'.(isset($_POST["fn-name"]) ? esc_attr($_POST["fn-name"]):'').'" size="35"/>';
+	echo '</p><p>';
+	echo 'Description (*) <br>';
+	echo '<textarea rows="4" cols="35" name="fn-desc">' . ( isset( $_POST["fn-desc"] ) ? esc_attr( $_POST["fn-desc"] ) : '' ) . '</textarea>';
+	echo '</p>';
+	echo '<p><input type="submit" name="fn-add" value="Add"/></p>';
+	echo '</form>';
+	echo '</div>';
+	
+	echo '</div>';
 }
 
 ?>
